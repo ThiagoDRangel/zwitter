ce6dfa2d78787058f4dad7dd230851a1c8238fbe
ThiagoDRangel <info.tec.campos@gmail.com>
2024-09-19 18:43:55 -0300

    feat: implements layout left page

---
+++ frontend/src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'Thiagodrangel',
+    name: 'Thiago Rangel',
+    avatar: 'https://www.flaticon.com/br/icone-gratis/avatar-de-perfil_4794936',
+    cover: 'https://pt.vecteezy.com/arte-vetorial/1882531-fundo-de-tecnologia-azul-escuro',
+    bio: 'Desenvolvedor Full Stack',
+    link: 'https://github.com/ThiagoDRangel',
+    postCount: 7
+}
