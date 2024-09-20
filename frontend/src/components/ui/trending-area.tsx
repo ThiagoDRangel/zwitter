f58d91a6ce60e5aae763c8385fc9042e1fd981ed
ThiagoDRangel <info.tec.campos@gmail.com>
2024-09-20 14:08:57 -0300

    feat: implements layout right page

--- frontend/src/components/ui/trending-area.tsx
+++ frontend/src/components/ui/trending-area.tsx
@@ -5,10 +5,10 @@ export const TrendingArea = () => {
         <div className="bg-gray-700 rounded-3xl">
             <h2 className="text-xl p-6">O que está acontecendo</h2>
             <div className="flex flex-col gap-4 p-6 pt-0">
-                <TrendingItem label="#test" count={1} />
-                <TrendingItem label="#test" count={1} />
-                <TrendingItem label="#test" count={2} />
-                <TrendingItem label="#test" count={3} />
+                <TrendingItem label="#Bitcoin" count={4975} />
+                <TrendingItem label="#tesla" count={5124} />
+                <TrendingItem label="#Brasil" count={85} />
+                <TrendingItem label="#Debate" count={354} />
                 <TrendingItemSkeleton />
                 <TrendingItemSkeleton />
             </div>
--- frontend/src/data/user.ts
+++ frontend/src/data/user.ts
@@ -3,7 +3,7 @@ import { User } from "@/types/user";
 export const user: User = {
     slug: 'Thiagodrangel',
     name: 'Thiago Rangel',
-    avatar: 'https://www.flaticon.com/br/icone-gratis/avatar-de-perfil_4794936',
+    avatar: 'https://i.ibb.co/SVKjmfj/profile.png',
     cover: 'https://pt.vecteezy.com/arte-vetorial/1882531-fundo-de-tecnologia-azul-escuro',
     bio: 'Desenvolvedor Full Stack',
     link: 'https://github.com/ThiagoDRangel',
